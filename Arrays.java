fff18892cc8ccc31460350b782668f1021a25bc5
hk2121 <149310231+hk2121@users.noreply.github.com>
2024-01-22 15:52:35 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        // int[] marks = new int[3];
+        // marks[0] = 99;
+        // marks[1] = 98;
+        // marks[2] = 97;
+
+        int marks[]= {97,98,95};
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Subject :   "+marks[i]);
+        }
+
+    }
+}
